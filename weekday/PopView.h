fcbcc3d6fab11ee118060d535918736f2217e61d
fudong_zhang <fudong_zhang@idea-source.net>
2017-06-14 17:57:22 +0800

    my first commit

---
+++ weekday/PopView.h
@@ -0,0 +1,13 @@
+//
+//  PopView.h
+//  weekday
+//
+//  Created by 上海点硕 on 2016/10/10.
+//  Copyright © 2016年 cbl－　点硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopView : UIView
+
+@end
---
+++ weekday/PopView.m
@@ -0,0 +1,21 @@
+//
+//  PopView.m
+//  weekday
+//
+//  Created by 上海点硕 on 2016/10/10.
+//  Copyright © 2016年 cbl－　点硕. All rights reserved.
+//
+
+#import "PopView.h"
+
+@implementation PopView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
